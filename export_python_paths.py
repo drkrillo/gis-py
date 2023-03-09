ce630ae5541d06448c85e082863880e8645226ae
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-03-09 13:57:07 -0300

    added script to export paths

--- export_python_paths.py
+++ export_python_paths.py
@@ -1,5 +1,11 @@
 import sys
 import pandas as pd
+from platform import python_version
+
+
+print(python_version())
+
 paths = sys.path
 df = pd.DataFrame({'paths':paths})
 df.to_csv('./qgis_sys_paths.csv', index=False)
+
